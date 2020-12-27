983ec02b253b1d7333dcc9bf4d3574cbe1562176
angkelmuthu <angkel.muthu@gmail.com>
2020-12-27 14:31:08 +0700

    update login

--- application/views/auth/login.php
+++ application/views/auth/login.php
@@ -2,91 +2,103 @@
 <html lang="en">
 
 <head>
+    <title>PEMKAB BOGOR | Login</title>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Kabupaten Bogor</title>
-    <link href="https://fonts.googleapis.com/css?family=Karla:400,700&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="https://cdn.materialdesignicons.com/4.8.95/css/materialdesignicons.min.css">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
-    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/Login/assets/css/login.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!--===============================================================================================-->
+    <link rel="icon" type="image/png" href="<?php echo base_url(); ?>assets/Login/images/icons/favicon.ico" />
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/Login/vendor/bootstrap/css/bootstrap.min.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/Login/fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/Login/fonts/Linearicons-Free-v1.0.0/icon-font.min.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/Login/vendor/animate/animate.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/Login/vendor/css-hamburgers/hamburgers.min.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/Login/vendor/select2/select2.min.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/Login/css/util.css">
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/Login/css/main.css">
+    <!--===============================================================================================-->
 </head>
 
 <body>
-    <main class="d-flex align-items-center min-vh-100 py-3 py-md-0">
-        <div class="container">
-            <div class="card login-card">
-                <div class="row no-gutters">
-                    <div class="col-md-8">
-                        <img src="<?php echo base_url(); ?>assets/Login/gambar.jpeg" alt="login" class="login-card-img">
-                    </div>
-                    <div class="col-md-4">
-                        <div class="card-body">
-                            <div class="brand-wrapper text-center">
-                                <img src="<?php echo base_url(); ?>assets/Login/logo.png" alt="logo" class="logo">
-                            </div>
-                            <?php
-                            $status_login = $this->session->userdata('status_login');
-                            if (empty($status_login)) {
-                                $message = "Silahkan login untuk masuk ke aplikasi";
-                            } else {
-                                $message = $status_login;
-                            }
-                            ?>
-                            <p class="login-card-description text-center"><?php echo $message; ?></p>
-                            <?php echo form_open('auth/cheklogin'); ?>
-                            <div class="form-group">
-                                <label for="email" class="sr-only">Username</label>
-                                <input type="text" name="full_name" id="username" class="form-control" placeholder="username">
-                            </div>
-                            <div class="form-group mb-4">
-                                <label for="password" class="sr-only">Password</label>
-                                <input type="password" name="password" id="password" class="form-control" placeholder="***********">
-                            </div>
-                            <button name="login" id="login" class="btn btn-block login-btn mb-4" type="submit">Login</button>
-                            </form>
-                            <!-- <a href="#!" class="forgot-password-link">Forgot password?</a>-->
 
-                            <!-- <p class="login-card-footer-text"></p> -->
-                            <!-- <nav class="login-card-footer-nav">
-                                <a href="#!">Terms of use.</a>
-                                <a href="#!">Privacy policy</a>
-                            </nav> -->
-                        </div>
-                    </div>
+    <div class="limiter">
+        <div class="container-login100" style="background-image: url('<?php echo base_url(); ?>assets/Login/gambar.jpeg');">
+            <div class="wrap-login100 p-t-10 p-b-30">
+                <?php
+                $status_login = $this->session->userdata('status_login');
+                if (empty($status_login)) {
+                    $message = "Silahkan login untuk masuk ke aplikasi";
+                } else {
+                    $message = $status_login;
+                }
+                ?>
+                <?php echo form_open('auth/cheklogin'); ?>
+                <div class="login100-form-avatar">
+                    <img src="<?php echo base_url(); ?>assets/Login/logo.png" alt="AVATAR">
                 </div>
+
+                <span class="login100-form-title p-t-20 p-b-45">
+                    PEMERINTAH KABUPATEN BOGOR
+                </span>
+
+                <div class="wrap-input100 validate-input m-b-10" data-validate="Username is required">
+                    <input class="input100" type="text" name="full_name" id="username" placeholder="Username">
+                    <span class="focus-input100"></span>
+                    <span class="symbol-input100">
+                        <i class="fa fa-user"></i>
+                    </span>
+                </div>
+
+                <div class="wrap-input100 validate-input m-b-10" data-validate="Password is required">
+                    <input class="input100" type="password" name="password" id="password" placeholder="Password">
+                    <span class="focus-input100"></span>
+                    <span class="symbol-input100">
+                        <i class="fa fa-lock"></i>
+                    </span>
+                </div>
+
+                <div class="container-login100-form-btn p-t-10">
+                    <button type="submit" class="login100-form-btn">
+                        Login
+                    </button>
+                </div>
+
+                <!-- <div class="text-center w-full p-t-25 p-b-230">
+                        <a href="#" class="txt1">
+                            Forgot Username / Password?
+                        </a>
+                    </div>
+
+                    <div class="text-center w-full">
+                        <a class="txt1" href="#">
+                            Create new account
+                            <i class="fa fa-long-arrow-right"></i>
+                        </a>
+                    </div> -->
+                </form>
             </div>
-            <!-- <div class="card login-card">
-        <img src="assets/images/login.jpg" alt="login" class="login-card-img">
-        <div class="card-body">
-          <h2 class="login-card-title">Login</h2>
-          <p class="login-card-description">Sign in to your account to continue.</p>
-          <form action="#!">
-            <div class="form-group">
-              <label for="email" class="sr-only">Email</label>
-              <input type="email" name="email" id="email" class="form-control" placeholder="Email">
-            </div>
-            <div class="form-group">
-              <label for="password" class="sr-only">Password</label>
-              <input type="password" name="password" id="password" class="form-control" placeholder="Password">
-            </div>
-            <div class="form-prompt-wrapper">
-              <div class="custom-control custom-checkbox login-card-check-box">
-                <input type="checkbox" class="custom-control-input" id="customCheck1">
-                <label class="custom-control-label" for="customCheck1">Remember me</label>
-              </div>
-              <a href="#!" class="text-reset">Forgot password?</a>
-            </div>
-            <input name="login" id="login" class="btn btn-block login-btn mb-4" type="button" value="Login">
-          </form>
-          <p class="login-card-footer-text">Don't have an account? <a href="#!" class="text-reset">Register here</a></p>
         </div>
-      </div> -->
-        </div>
-    </main>
-    <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+    </div>
+
+
+
+
+    <!--===============================================================================================-->
+    <script src="<?php echo base_url(); ?>assets/Login/vendor/jquery/jquery-3.2.1.min.js"></script>
+    <!--===============================================================================================-->
+    <script src="<?php echo base_url(); ?>assets/Login/vendor/bootstrap/js/popper.js"></script>
+    <script src="<?php echo base_url(); ?>assets/Login/vendor/bootstrap/js/bootstrap.min.js"></script>
+    <!--===============================================================================================-->
+    <script src="<?php echo base_url(); ?>assets/Login/vendor/select2/select2.min.js"></script>
+    <!--===============================================================================================-->
+    <script src="<?php echo base_url(); ?>assets/Login/js/main.js"></script>
+
 </body>
 
 </html>
