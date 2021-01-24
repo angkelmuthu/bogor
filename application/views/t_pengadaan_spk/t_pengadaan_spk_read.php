fb8c47e79a913f898d693dccad720296522d5f49
angkelmuthu <angkel.muthu@gmail.com>
2021-01-24 22:18:27 +0700

    update pengadaan spk tombol

--- application/views/t_pengadaan_spk/t_pengadaan_spk_read.php
+++ application/views/t_pengadaan_spk/t_pengadaan_spk_read.php
@@ -65,7 +65,10 @@
 				<div class="panel-container show">
 					<div class="panel-content">
 						<div class="text-center">
-							<?php echo anchor(site_url('t_pengadaan_spk/create_detail/' . $this->uri->segment(3)), '<i class="fal fa-plus-square" aria-hidden="true"></i> Tambah Data', 'class="btn btn-primary btn-sm waves-effect waves-themed"'); ?></div>
+							<?php
+							if ($this->session->userdata('kode_jenis_unit') != 3) {
+								echo anchor(site_url('t_pengadaan_spk/create_detail/' . $this->uri->segment(3)), '<i class="fal fa-plus-square" aria-hidden="true"></i> Tambah Data', 'class="btn btn-primary btn-sm waves-effect waves-themed"');
+							} ?></div>
 						<table class="table table-bordered table-hover table-striped w-100" id="dt-basic-example">
 							<thead>
 								<tr>
